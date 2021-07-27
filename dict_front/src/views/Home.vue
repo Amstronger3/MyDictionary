a99efc5674cab13fabce60a28946c9ea846f84af
aleksandr <amstronger3@gmai.com>
2021-07-27 15:32:35 +0300

    master

---
+++ dict_front/src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+        
+    <v-btn large color="error">hell</v-btn>
+    <v-data-table> </v-data-table>
+    <div v-for="word in words" :key="word.id">
+      {{ word.original_word }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      words: [],
+    };
+  },
+
+  components: {
+  },
+  mounted() {
+    this.getWords();
+  },
+  methods: {
+    getWords() {
+      axios
+        .get("/api/v1/words/")
+        .then((resp) => {
+          this.words = resp.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
