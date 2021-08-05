43a6e6f22f2db587de9dfb7a721c17387fbd988a
aleksandr <amstronger3@gmai.com>
2021-08-05 17:23:45 +0300

    CONFIG

--- dict_front/vue.config.js
+++ dict_front/vue.config.js
@@ -12,9 +12,3 @@ module.exports = {
     }
   }
 }
-
-
-https://certificate.ithillel.ua/view/53468618
-https://www.sololearn.com/Certificate/1073-16851174/pdf/
-https://github.com/Amstronger3
-http://linkedin.com/in/apostolovleks
